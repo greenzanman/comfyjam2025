728208e6bd086d920889984ece1dcc497eb56230
Raaee <99899402+Raaee@users.noreply.github.com>
2025-11-04 01:09:21 -0500

    inventory ui

--- ComfyJam2025/Assets/Scripts/UI/InventoryUI.cs
+++ ComfyJam2025/Assets/Scripts/UI/InventoryUI.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    [SerializeField] private PlayerManager playerManager;
     [SerializeField] private GameObject inventoryPanel;
+
+    public List<ItemSlotUI> itemSlotUIs;
+
     private bool isOpen = false;
+
+    private void Start() {
+        PlayerManager.instance.OnItemChange.AddListener(UpdateUI);
+    }
 
     public void ToggleInventory() {
         isOpen = !isOpen;
         inventoryPanel.SetActive(isOpen);
     }
 
-    
+    public void UpdateUI(ItemType itemType) {
+        ItemData ID = PlayerManager.instance.possibleItems.FirstOrDefault(searching => searching.itemType == itemType);
+        int indexOfID = PlayerManager.instance.possibleItems.IndexOf(ID);
+        itemSlotUIs[indexOfID].gameObject.SetActive(true);
+        itemSlotUIs[indexOfID].UpdateUI(ID.itemSprite, PlayerManager.instance.inventory[ID.itemType]);
+    }
 }
